1e691bbfc711216a63ad1c73bfb9fd5d665e4b8a
Albert Robrigado <albertrobrigado.ict@gmail.com>
2026-04-12 10:35:13 +0800

    refactor(responsive): adjust the font size of the text in the logo carousel section when it reaches the 900px and 700px. refactor(responsive): reduce the opacity of the recognized by label to 0 when the screen size is below 700px

--- src/components/layout/LogoCarouselSection.jsx
+++ src/components/layout/LogoCarouselSection.jsx
@@ -3,21 +3,22 @@ import BrandCarousel from "../animations/BrandCarousel.jsx";
 export default function LogoCarouselSection() {
     return (
         <section className="w-full px-10 py-10">
-            <div className="flex flex-col gap-20">
+            <div className="flex flex-col min-[700px]:gap-15 tablet:gap-20">
                 <div className="text-[clamp(3rem,5vw,4.5rem)] leading-[0.9] font-bold">
                     <p className="mb-1 text-sm font-semibold text-[#7F7F7F]">Partners / Affiliation</p>
                     <h3>Backed by</h3>
                     <h3>trusted institutions</h3>
                 </div>
-                <div className="flex justify-between">
-                    <p className="font-semibold tracking-[0.025rem] text-[clamp(0.65rem,1vw,0.80rem)]">Recognized by</p>
-                    <p className="font-medium max-w-2xl tracking-[0.025rem] text-[clamp(1rem,1.2vw,1.375rem)]">
+                <div className="flex flex-col gap-4 pb-12 min-[700px]:pb-0 min-[700px]:flex-row min-[700px]:justify-between">
+                    <p className="opacity-0 min-[700px]:opacity-100 min-[700px]:font-semibold min-[700px]:tracking-[0.025rem] min-[700px]:text-[clamp(0.6rem,2.2vw,0.75rem)] tablet:text-[clamp(0.65rem,1vw,0.80rem)]">
+                        Recognized by
+                    </p>
+                    <p className="font-medium max-w-full tracking-[0.025rem] text-[clamp(1rem,1.5vw,1.375rem)] min-[700px]:max-w-md tablet:max-w-2xl">
                         Powerkicks is aligned with recognized national and international organizations, ensuring
                         structured training, certified standards, and consistent athlete development. </p>
                 </div>
-                <div>
-                    <BrandCarousel></BrandCarousel>
-                </div>
+                {/* Brand Carousel */}
+                <BrandCarousel></BrandCarousel>
             </div>
         </section>
     )
