2281e33477bf06a0c79012a2f9cade4473fcba58
Albert Robrigado <albertrobrigado.ict@gmail.com>
2026-05-02 09:04:13 +0800

    feat: add some additional content in the testimonial section.

--- src/components/sections/testimonial/TestimonialSection.jsx
+++ src/components/sections/testimonial/TestimonialSection.jsx
@@ -1,7 +1,32 @@
+import BrandCarousel from "../../animations/BrandCarousel.jsx";
+
 export default function TestimonialSection() {
     return(
         <section className="w-full px-10 pt-[2rem] pb-[2rem]">
-            <p>Testimonial Section</p>
+            {/* Header */}
+            <div className="flex flex-col min-[700px]:gap-15 tablet:gap-20">
+                <div className="text-[clamp(3rem,5vw,4.5rem)] leading-[0.9] font-bold pb-6">
+                    <p className="mb-1 text-sm font-semibold text-[#7F7F7F]">Testimonials</p>
+                    <h3>Champion Share</h3>
+                    <h3>Their Stories</h3>
+                </div>
+                <div
+                    className="flex flex-col gap-4 pb-12 min-[700px]:pb-0 min-[700px]:flex-row min-[700px]:justify-between"
+                >
+                    <p className="opacity-0
+                                  min-[700px]:opacity-100
+                                  min-[700px]:font-semibold
+                                  min-[700px]:tracking-[0.025rem]
+                                  min-[700px]:text-[0.80rem]
+                                  tablet:text-[clamp(0.80rem,1.25vw,1rem)]
+                                ">
+                        Achievements
+                    </p>
+                    <p className="font-medium max-w-full tracking-[0.025rem] text-[clamp(0.85rem,1.5vw,1rem)] min-[700px]:max-w-md tablet:max-w-2xl">
+                        If you're looking for disciplined, experienced, and high-quality taekwondo coaches
+                        you're in the right place. </p>
+                </div>
+            </div>
         </section>
     );
 }
