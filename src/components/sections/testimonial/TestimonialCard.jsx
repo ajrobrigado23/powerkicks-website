d667d6fd36c72257a85cc800d2b7d0af9b53bb36
Albert Robrigado <albertrobrigado.ict@gmail.com>
2026-05-07 16:38:42 +0800

    feat: add additional contents like star icon, quote icon and fix font size and style.

--- src/components/sections/testimonial/TestimonialCard.jsx
+++ src/components/sections/testimonial/TestimonialCard.jsx
@@ -1,23 +1,26 @@
 import togashiImg from "../../../assets/images/togashi.jpg";
+import { Star, Quote } from "lucide-react"
 
 export default function TestimonialCard() {
 
     return (
         <div className="flex flex-col gap-4">
-            <p>quotes</p>
-            <p className="font-medium max-w-full tracking-[0.025rem] text-[clamp(0.85rem,1.5vw,1rem)]">
-                “We build Discipline that carries into school and work. We push limits. We demand focus. We celebrate
-                growth.”</p>
-            <hr className="border-t border-gray-300"/>
+            <Quote />
+            <p className="font-regular italic max-w-full tracking-[0.025rem] text-[clamp(0.85rem,1.5vw,1.15rem)]">
+                We build Discipline that carries into school and work. We push limits. We demand focus. We celebrate
+                growth.</p>
+            <hr className="border-t w-10 "/>
             {/* details section */}
             <div className="flex gap-2">
                 <img
                     className="h-15 w-15 object-cover object-center rounded-full"
                     src={togashiImg} alt=""/>
                 <div className="flex flex-col">
-                    <p>stars</p>
-                    <p>Albert Joshua T. Robrigado</p>
-                    <p>Member of Junior National Team</p>
+                    <Star size={16} color="#B91C1C"/>
+                    <p className="font-medium italic tracking-[0.025rem] text-[clamp(0.65rem,1.5vw,0.85rem)] pt-2">
+                        Albert Joshua T. Robrigado</p>
+                    <p className="font-medium tracking-[0.025rem] text-[clamp(0.65rem,1.5vw,0.75rem)] text-[#7F7F7F]">
+                        Member of Junior National Team</p>
                 </div>
             </div>
         </div>
