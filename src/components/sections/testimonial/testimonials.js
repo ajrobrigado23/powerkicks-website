cf3fc684b4fcec21a3c2c0a81a5b6f24f6b35e14
Albert Robrigado <albertrobrigado.ict@gmail.com>
2026-05-13 18:17:37 +0800

    feat: create a data for all testimonials.

---
+++ src/components/sections/testimonial/testimonials.js
@@ -0,0 +1,46 @@
+import imageOne from "../../../assets/images/togashi.jpg";
+import imageTwo from "../../../assets/images/asukasa.jpg";
+import imageThree from "../../../assets/images/frieren.jpg";
+import imageFour from "../../../assets/images/himmel.jpg";
+import imageFive from "../../../assets/images/kaidou.jpg";
+import imageSix from "../../../assets/images/komiya.jpg";
+
+export const testimonials = [
+    {
+        name: "Albert Joshua T. Robrigado",
+        title: "Member of Junior National Team",
+        quotes: "We build Discipline that carries into school and work. We push limits. We demand focus. We celebrate growth.",
+        img:imageOne,
+    },
+    {
+        name: "Albert Joshua T. Robrigado",
+        title: "Member of Junior National Team",
+        quotes: "We build Discipline that carries into school and work. We push limits. We demand focus. We celebrate growth.",
+        img:imageTwo,
+    },
+    {
+        name: "Albert Joshua T. Robrigado",
+        title: "Member of Junior National Team",
+        quotes: "We build Discipline that carries into school and work. We push limits. We demand focus. We celebrate growth.",
+        img:imageThree,
+    },
+    {
+        name: "Albert Joshua T. Robrigado",
+        title: "Member of Junior National Team",
+        quotes: "We build Discipline that carries into school and work. We push limits. We demand focus. We celebrate growth.",
+        img:imageFour,
+    },
+    {
+        name: "Albert Joshua T. Robrigado",
+        title: "Member of Junior National Team",
+        quotes: "We build Discipline that carries into school and work. We push limits. We demand focus. We celebrate growth.",
+        img:imageFive,
+    },
+    {
+        name: "Albert Joshua T. Robrigado",
+        title: "Member of Junior National Team",
+        quotes: "We build Discipline that carries into school and work. We push limits. We demand focus. We celebrate growth.",
+        img:imageSix,
+    },
+
+];
