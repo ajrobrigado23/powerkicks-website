3da344a572457425405ff2fdddc1f2977202eac4
Albert Robrigado <albertrobrigado.ict@gmail.com>
2026-05-25 07:44:48 +0800

    refactor: group testimonial cards by page

--- src/components/sections/testimonial/TestimonialCarousel.jsx
+++ src/components/sections/testimonial/TestimonialCarousel.jsx
@@ -3,6 +3,14 @@ import { testimonials } from "./testimonials.js";
 
 export default function TestimonialCarousel({ currentPage }) {
 
+    // render each group as one full page
+    const groupedTestimonials = [];
+
+    for (let i = 0; i < testimonials.length; i += 3) {
+        // move from page 1 to page 2, instead of sliding card by card.
+        groupedTestimonials.push(testimonials.slice(i, i + 3));
+    }
+
     return(
         <div className="h-full w-full overflow-hidden">
             <div
@@ -10,24 +18,30 @@ export default function TestimonialCarousel({ currentPage }) {
                 style={{ transform: `translateX(-${currentPage * 100}%)` }}
             >
                 {
-                    testimonials.map((testimonial, index) => {
-                        return (
-                            <div
-                                className="w-1/3 shrink-0 h-full"
-                                key={index}
-                            >
-                                <TestimonialCard
-                                    name={testimonial.name}
-                                    title={testimonial.title}
-                                    quotes={testimonial.quotes}
-                                    image={testimonial.img}
-                                />
-                            </div>
-                        );
-                    })
+                    groupedTestimonials.map((group, pageIndex) => (
+                        <div
+                            key={pageIndex}
+                            className="flex h-full min-w-full"
+                        >
+                            {
+                                group.map((testimonial, index) => (
+                                <div
+                                    key={index}
+                                    className="h-full w-1/3 shrink-0"
+                                >
+                                    <TestimonialCard
+                                        name={testimonial.name}
+                                        title={testimonial.title}
+                                        quotes={testimonial.quotes}
+                                        image={testimonial.img}
+                                    />
+                                </div>
+                            ))
+                            }
+                        </div>
+                    ))
                 }
             </div>
-
         </div>
     );
 }
