63fc3ae1433662cc3e6d570d457ebee8cb216949
Albert Robrigado <albertrobrigado.ict@gmail.com>
2026-06-04 08:05:22 +0800

    content: add FAQ section heading and intro

---
+++ src/components/sections/faq/FAQAccordionItem.jsx
@@ -0,0 +1,3 @@
+export default function FAQAccordionItem() {
+    return null;
+}
--- src/components/sections/faq/FAQSection.jsx
+++ src/components/sections/faq/FAQSection.jsx
@@ -1,8 +1,32 @@
 export default function FAQSection () {
 
     return (
-        <section className="w-full px-10 pt-[2rem] pb-[2rem]">
-
+        <section className="w-full px-10 pt-[2rem] pb-[2rem] bg-black text-white">
+            <div className="flex flex-col min-[700px]:gap-15 tablet:gap-20">
+                {/* Section heading */}
+                <div className="text-[clamp(3rem,5vw,4.5rem)] leading-[0.9] font-bold pb-6">
+                    <p className="mb-1 text-sm font-semibold text-[#7F7F7F]">Partners / Affiliation</p>
+                    <h3>Frequently asked</h3>
+                    <h3>questions</h3>
+                </div>
+                {/* FAQ content section */}
+                <div
+                    className="flex flex-col gap-4 pb-12 min-[700px]:pb-0 min-[700px]:flex-row min-[700px]:justify-between"
+                >
+                    <p className="opacity-0
+                                  min-[700px]:opacity-100
+                                  min-[700px]:font-semibold
+                                  min-[700px]:tracking-[0.025rem]
+                                  min-[700px]:text-[0.80rem]
+                                  tablet:text-[clamp(0.80rem,1.25vw,1rem)]
+                                ">
+                        Recognized by
+                    </p>
+                    <p className="font-medium max-w-full tracking-[0.025rem] text-[clamp(0.85rem,1.5vw,1rem)] min-[700px]:max-w-md tablet:max-w-2xl">
+                        Powerkicks is aligned with recognized national and international organizations, ensuring
+                        structured training, certified standards, and consistent athlete development. </p>
+                </div>
+            </div>
         </section>
     );
 }
