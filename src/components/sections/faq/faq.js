b9352ea59dbd2244e4b45105e75b7e4ce3e0c6eb
Albert Robrigado <albertrobrigado.ict@gmail.com>
2026-06-05 07:13:35 +0800

    data: add FAQ content data

---
+++ src/components/sections/faq/faq.js
@@ -0,0 +1,18 @@
+export const faq = [
+    {
+        question: "How is Kyoso different from other creative AI tools? ",
+        answer:"We build Discipline that carries into school and work. We push limits. We demand focus. We celebrate growth.",
+    },
+    {
+        question: "How is Kyoso different from other creative AI tools? ",
+        answer:"We build Discipline that carries into school and work. We push limits. We demand focus. We celebrate growth.",
+    },
+    {
+        question: "How is Kyoso different from other creative AI tools? ",
+        answer:"We build Discipline that carries into school and work. We push limits. We demand focus. We celebrate growth.",
+    },
+    {
+        question: "How is Kyoso different from other creative AI tools? ",
+        answer:"We build Discipline that carries into school and work. We push limits. We demand focus. We celebrate growth.",
+    },
+];
