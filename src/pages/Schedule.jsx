a27fb0e5f7b6d361e5f4e9fe0083ec0ace81936a
Albert Robrigado <albertrobrigado.ict@gmail.com>
2026-06-23 10:43:26 +0800

    feat: scaffold schedule page layout

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -1,8 +1,14 @@
+import NavBar from "../components/layout/NavBar.jsx";
+
 export default function Schedule() {
 
     return (
-        <main>
-            <h1>Training Schdule</h1>
-        </main>
+        <>
+            <NavBar></NavBar>
+
+            <main>
+
+            </main>
+        </>
     );
 }
