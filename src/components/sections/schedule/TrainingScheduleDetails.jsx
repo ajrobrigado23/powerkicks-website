09405b0928843d6934c7601993e31d713125e3ef
Albert Robrigado <albertrobrigado.ict@gmail.com>
2026-08-09 23:36:17 +0800

    feat: add TextReveal paragraph to training schedule details

--- src/components/sections/schedule/TrainingScheduleDetails.jsx
+++ src/components/sections/schedule/TrainingScheduleDetails.jsx
@@ -4,7 +4,18 @@ import ScheduleDay from "./ScheduleDay.jsx";
 export default function TrainingScheduleDetails({ className, location }) {
     return (
         <>
-            <h4 className="text-[clamp(0.90rem,1.25vw,1.25rem)] leading-[0.9] font-bold uppercase pb-10">Training Schedule</h4>
+            <TextReveal
+                as="h4"
+                type="words"
+                triggerOnScroll
+                scrollStart="top 85%"
+                className="text-[clamp(0.90rem,1.25vw,1.25rem)] leading-[0.9] font-bold uppercase pb-10"
+                duration={1.2}
+                delay={0.3}
+                stagger={0.07}
+            >
+                Training Schedule
+            </TextReveal>
 
             <div className={className}>
                 {location === "dasmarinas" && (
@@ -38,14 +49,23 @@ export default function TrainingScheduleDetails({ className, location }) {
                     </>
                 )}
             </div>
-
-            <p className="py-20">
+            <TextReveal
+                as="h4"
+                type="words"
+                triggerOnScroll
+                scrollStart="top 85%"
+                className="py-20"
+                duration={0.6}
+                delay={0.2}
+                stagger={0.02}
+            >
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
                 industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the
                 librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to
                 make dummy text for Letraset's Body Type sheets. It has survived not only many decades, but also the
                 leap into electronic typesetting, remaining essentially unchanged.
-            </p>
+            </TextReveal>
+
         </>
 
     );
