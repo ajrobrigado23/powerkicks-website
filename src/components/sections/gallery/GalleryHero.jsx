b70f0c81d8bdeb32e8e0bacbf130e70657a1bf0d
Albert Robrigado <albertrobrigado.ict@gmail.com>
2026-08-26 15:59:52 +0800

    feat: add gallery image data configuration and complete the remaining gallery hero content

--- src/components/sections/gallery/GalleryHero.jsx
+++ src/components/sections/gallery/GalleryHero.jsx
@@ -1,11 +1,15 @@
 import TextReveal from "../../animations/TextReveal.jsx";
+import SchedulePicture from "../schedule/SchedulePicture.jsx";
+import {galleryImages} from "../gallery/gallery.js";
 
 export default function GalleryHero() {
+
+    const firstImage = galleryImages[0];
 
     return (
         <section className="grid grid-cols-12 px-10 pt-[10rem]">
             {/* gallery hero section */}
-            <div className="col-span-5">
+            <div className="col-span-4">
                 {/* sub header */}
                 <TextReveal
                     as="h3"
@@ -18,7 +22,7 @@ export default function GalleryHero() {
                     Gallery
                 </TextReveal>
             </div>
-            <div className="col-span-7">
+            <div className="col-span-8">
                 {/* paragraph */}
                 <TextReveal
                     as="p"
@@ -30,6 +34,44 @@ export default function GalleryHero() {
                 >
                     A visual story of Powerkicks Taekwondo capturing our students, coaches, training sessions,
                     competitions, <span className="text-[#7F7F7F]">and the moments that define our discipline-driven community.</span>
+                </TextReveal>
+
+                {/* picture */}
+                <SchedulePicture
+                    src={firstImage.src}
+                    alt={firstImage.alt}
+                    className="h-[800px] w-full py-2"
+                    centerPercentage={firstImage.centerPercentage}
+                />
+            </div>
+
+            {/* 2nd row */}
+            <div className="col-span-4 pt-10">
+                {/* sub header */}
+                <TextReveal
+                    as="h3"
+                    type="words"
+                    className="text-[clamp(0.90rem,1.25vw,1.25rem)] leading-[0.9] font-bold uppercase"
+                    duration={1.6}
+                    delay={1}
+                    stagger={0.07}
+                >
+                    Our Mission
+                </TextReveal>
+            </div>
+            <div className="col-span-8 pt-10 pb-15">
+                {/* paragraph */}
+                <TextReveal
+                    as="p"
+                    type="words"
+                    className=""
+                    triggerOnScroll
+                    duration={1.2}
+                    delay={1.25}
+                    stagger={0.025}
+                >
+                    Our mission is to help students grow through disciplined taekwondo training building confidence,
+                    focus, respect, and strength that extends beyond the mat.
                 </TextReveal>
             </div>
 
---
+++ src/components/sections/gallery/gallery.js
@@ -0,0 +1,10 @@
+import scheduleImgOne from "../../../assets/images/gallery-images/IMG_7550.JPG";
+
+export const galleryImages = [
+    {
+        id: 1,
+        src: scheduleImgOne,
+        alt: "Powerkicks training gallery image 1",
+        centerPercentage: "20%",
+    },
+];
