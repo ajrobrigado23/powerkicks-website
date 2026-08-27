334381dcf81514691dbbe31c9a5066fb20b6f324
Albert Robrigado <albertrobrigado.ict@gmail.com>
2026-08-27 08:08:22 +0800

    feat: add full-width image section with overlay and text to gallery hero

--- src/components/sections/gallery/GalleryHero.jsx
+++ src/components/sections/gallery/GalleryHero.jsx
@@ -1,13 +1,14 @@
 import TextReveal from "../../animations/TextReveal.jsx";
 import SchedulePicture from "../schedule/SchedulePicture.jsx";
-import {galleryImages} from "../gallery/gallery.js";
+import { galleryImages } from "../gallery/gallery.js";
 
 export default function GalleryHero() {
 
     const firstImage = galleryImages[0];
+    const secondImage = galleryImages[1];
 
     return (
-        <section className="grid grid-cols-12 px-10 pt-[10rem]">
+        <section className="grid grid-cols-12 px-10 pt-[12rem]">
             {/* gallery hero section */}
             <div className="col-span-4">
                 {/* sub header */}
@@ -59,7 +60,7 @@ export default function GalleryHero() {
                     Our Mission
                 </TextReveal>
             </div>
-            <div className="col-span-8 pt-10 pb-15">
+            <div className="col-span-8 pt-10 pb-10">
                 {/* paragraph */}
                 <TextReveal
                     as="p"
@@ -67,12 +68,34 @@ export default function GalleryHero() {
                     className=""
                     triggerOnScroll
                     duration={1.2}
-                    delay={1.25}
                     stagger={0.025}
                 >
                     Our mission is to help students grow through disciplined taekwondo training building confidence,
                     focus, respect, and strength that extends beyond the mat.
                 </TextReveal>
+            </div>
+            {/* step by step training journey */}
+            <div className="col-span-12 py-10 -mx-10">
+                <div className="relative overflow-hidden h-[750px]">
+                    {/* Image */}
+                    <img
+                        src={secondImage.src}
+                        alt=""
+                        className="absolute inset-0 z-0 h-full w-full object-cover object-[center_40%]"
+                    />
+
+                    {/* Dark filter */}
+                    <div className="absolute inset-0 z-10 bg-black/50" />
+
+                    {/* Text */}
+                    <h3 className="absolute top-10 left-10 z-20 text-white text-[clamp(0.90rem,1.25vw,1.25rem)] leading-[0.9] font-semibold uppercase">
+                        Our
+                        <br />
+                        Step-by-Step
+                        <br/>Training Journey
+                    </h3>
+                </div>
+
             </div>
 
         </section>
--- src/components/sections/gallery/gallery.js
+++ src/components/sections/gallery/gallery.js
@@ -1,10 +1,17 @@
 import scheduleImgOne from "../../../assets/images/gallery-images/IMG_7550.JPG";
+import scheduleImgTwo from "../../../assets/images/gallery-images/IMG_7594.JPG";
 
 export const galleryImages = [
     {
         id: 1,
         src: scheduleImgOne,
         alt: "Powerkicks training gallery image 1",
+        centerPercentage: "20%",
+    },
+    {
+        id: 2,
+        src: scheduleImgTwo,
+        alt: "Powerkicks training gallery image 2",
         centerPercentage: "20%",
     },
 ];
