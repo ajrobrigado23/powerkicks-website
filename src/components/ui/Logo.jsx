e0d62dbc92c51e63f01c2800f2690147cf3f1049
Albert Robrigado <albertrobrigado.ict@gmail.com>
2026-09-08 09:47:11 +0800

    feat: add white Powerkicks logo asset

--- src/components/ui/Logo.jsx
+++ src/components/ui/Logo.jsx
@@ -1,14 +1,12 @@
 import powerkicksLogoBlack from "/logo/powerkicks-black-logo.png";
 import powerkicksLogoWhite from "/logo/powerkicks-white-logo.png";
 
-export default function Logo() {
+export default function Logo({ isBlack}) {
 
     return (
-        <div>
-            <img
-                src={powerkicksLogoBlack}
-                className="w-14 h-auto"
-                alt="Powerkicks Black Logo" />
-        </div>
+        <img
+            src={ isBlack ?  powerkicksLogoWhite : powerkicksLogoBlack}
+            className="w-14 h-auto"
+            alt="Powerkicks Black Logo" />
     );
 }
