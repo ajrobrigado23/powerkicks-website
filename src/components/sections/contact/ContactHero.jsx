dd7b77b3888c87b34f4e2353a25ab1fb9f7003b5
Albert Robrigado <albertrobrigado.ict@gmail.com>
2026-09-12 16:47:52 +0800

    feat: add Contact heading to ContactHero component

--- src/components/sections/contact/ContactHero.jsx
+++ src/components/sections/contact/ContactHero.jsx
@@ -1,7 +1,20 @@
+import TextReveal from "../../animations/TextReveal.jsx";
+
 export default function ContactHero() {
 
     return(
-        <section className="">
+        <section className="grid grid-cols-12 px-10 pt-[10rem] w-full">
+            <div className="col-span-12 text-white">
+                <TextReveal
+                    as="h1"
+                    type="letters"
+                    className="uppercase text-[clamp(6rem,16vw,24rem)] font-bold tracking-[-0.30rem] leading-none whitespace-nowrap text-white"
+                    duration={2.50}
+                    stagger={0.07}
+                >
+                    Contact
+                </TextReveal>
+            </div>
 
         </section>
     );
